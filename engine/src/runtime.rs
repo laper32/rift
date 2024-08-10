e6f152a13d529f73cdf5944d43768508dc75bbe4
laper32 <laper32@outlook.com>
2024-08-10 22:16:18 +0800

    feat: runtime initialization

--- engine/src/runtime.rs
+++ engine/src/runtime.rs
@@ -1,70 +1,142 @@
-// static PLATFORM: SharedRef<Platform> = v8::new_default_platform(0, false).make_shared();
 
+use deno_ast::{MediaType, ParseParams, SourceTextInfo};
+use deno_core::anyhow::Error;
+use deno_core::error::AnyError;
+use deno_core::{extension, op2, FastString, ModuleLoadResponse, ModuleSourceCode, ModuleSpecifier, ModuleType, ResolutionKind};
+use std::env;
+use std::rc::Rc;
+
+#[op2(async)]
+#[string]
+async fn op_read_file(#[string] path: String) -> Result<String, AnyError> {
+    let contents = tokio::fs::read_to_string(path).await?;
+    Ok(contents)
+}
+
+#[op2(async)]
+#[string]
+async fn op_write_file(#[string] path: String, #[string] contents: String) -> Result<(), AnyError> {
+    tokio::fs::write(path, contents).await?;
+    Ok(())
+}
+
+#[op2(async)]
+#[string]
+async fn op_fetch(#[string] url: String) -> Result<String, AnyError> {
+    let body = reqwest::get(url).await?.text().await?;
+    Ok(body)
+}
+
+#[op2(async)]
+async fn op_set_timeout(delay: f64) -> Result<(), AnyError> {
+    tokio::time::sleep(std::time::Duration::from_millis(delay as u64)).await;
+    Ok(())
+}
+
+#[op2(fast)]
+fn op_remove_file(#[string] path: String) -> Result<(), AnyError> {
+    std::fs::remove_file(path)?;
+    Ok(())
+}
+
+struct TsModuleLoader;
+
+impl deno_core::ModuleLoader for TsModuleLoader {
+    fn resolve(
+        &self,
+        specifier: &str,
+        referrer: &str,
+        _: ResolutionKind,
+    ) -> Result<ModuleSpecifier, Error> {
+        deno_core::resolve_import(specifier, referrer).map_err(|e| e.into())
+    }
+
+    fn load(
+        &self,
+        module_specifier: &ModuleSpecifier,
+        _maybe_referrer: Option<&reqwest::Url>,
+        _is_dyn_import: bool,
+        _requested_module_type: deno_core::RequestedModuleType,
+    ) -> ModuleLoadResponse {
+        let module_specifier = module_specifier.clone();
+        let module_load = Box::pin(async move {
+            let path = module_specifier.to_file_path().unwrap();
+
+            let media_type = MediaType::from_path(&path);
+
+            let module_type = match media_type {
+                MediaType::JavaScript
+                | MediaType::Mjs
+                | MediaType::Cjs
+                | MediaType::Jsx
+                | MediaType::TypeScript
+                | MediaType::Mts
+                | MediaType::Dts
+                | MediaType::Dmts
+                | MediaType::Dcts
+                | MediaType::Tsx => ModuleType::JavaScript,
+                MediaType::Json => ModuleType::Json,
+                _ => panic!("Unknown extension: {:?}", path.extension()),
+            };
+            let code = std::fs::read_to_string(&path)?;
+            let parsed_source = deno_ast::parse_module(ParseParams {
+                specifier: module_specifier.clone(),
+                text: SourceTextInfo::from_string(code).text(),
+                media_type,
+                capture_tokens: false,
+                scope_analysis: false,
+                maybe_syntax: None,
+            })?;
+
+            let module = deno_core::ModuleSource::new(
+                module_type,
+                ModuleSourceCode::String(FastString::from(parsed_source.text().clone())),
+                &module_specifier,
+                None,
+            );
+            Ok(module)
+        });
+        ModuleLoadResponse::Async(module_load)
+    }
+}
+static RUNTIME_SNAPSHOT: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/RUNJS_SNAPSHOT.bin"));
+
+extension! {
+    runjs,
+    ops = [
+        op_read_file,
+        op_write_file,
+        op_remove_file,
+        op_fetch,
+        op_set_timeout,
+    ]
+}
+async fn run_js(file_path: &str) -> Result<(), AnyError> {
+    let main_module = deno_core::resolve_path(file_path, env::current_dir()?.as_path())?;
+    let mut js_runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions {
+        module_loader: Some(Rc::new(TsModuleLoader)),
+        startup_snapshot: Some(RUNTIME_SNAPSHOT),
+        extensions: vec![runjs::init_ops()],
+        ..Default::default()
+    });
+
+    let mod_id = js_runtime.load_main_es_module(&main_module).await?;
+    let result = js_runtime.mod_evaluate(mod_id);
+    js_runtime.run_event_loop(Default::default()).await?;
+    result.await
+}
 pub fn init() {
-    // Initialize V8.
-    // v8::V8::initialize_platform(PLATFORM);
-    // v8::V8::initialize();
-    // {
-    //     // Create a new Isolate and make it the current one.
-    //     let isolate = &mut v8::Isolate::new(v8::CreateParams::default());
-
-    //     // Create a stack-allocated handle scope.
-    //     let handle_scope = &mut v8::HandleScope::new(isolate);
-
-    //     // Create a new context.
-    //     let context = v8::Context::new(handle_scope, Default::default());
-
-    //     // Enter the context for compiling and running the hello world script.
-    //     let scope = &mut v8::ContextScope::new(handle_scope, context);
-
-    //     // Create a string containing the JavaScript source code.
-    //     let code = v8::String::new(scope, "'Hello' + ' World!'").unwrap();
-
-    //     // Compile the source code.
-    //     let script = v8::Script::compile(scope, code, None).unwrap();
-    //     // Run the script to get the result.
-    //     let result = script.run(scope).unwrap();
-
-    //     // Convert the result to a string and print it.
-    //     let result = result.to_string(scope).unwrap();
-    //     println!("{}", result.to_rust_string_lossy(scope));
-
-    //     // Use the JavaScript API to generate a WebAssembly module.
-    //     //
-    //     // |bytes| contains the binary format for the following module:
-    //     //
-    //     //     (func (export "add") (param i32 i32) (result i32)
-    //     //       get_local 0
-    //     //       get_local 1
-    //     //       i32.add)
-    //     //
-    //     let c_source = r#"
-    //       let bytes = new Uint8Array([
-    //         0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, 0x01, 0x07, 0x01,
-    //         0x60, 0x02, 0x7f, 0x7f, 0x01, 0x7f, 0x03, 0x02, 0x01, 0x00, 0x07,
-    //         0x07, 0x01, 0x03, 0x61, 0x64, 0x64, 0x00, 0x00, 0x0a, 0x09, 0x01,
-    //         0x07, 0x00, 0x20, 0x00, 0x20, 0x01, 0x6a, 0x0b
-    //       ]);
-    //       let module = new WebAssembly.Module(bytes);
-    //       let instance = new WebAssembly.Instance(module);
-    //       instance.exports.add(3, 4);
-    //     "#;
-    //     // Create a string containing the JavaScript source code.
-    //     let source = v8::String::new(scope, c_source).unwrap();
-
-    //     // Compile the source code.
-    //     let script = v8::Script::compile(scope, source, None).unwrap();
-
-    //     // Run the script to get the result.
-    //     let result = script.run(scope).unwrap();
-
-    //     // Print the result.
-    //     let result = result.to_uint32(scope).unwrap();
-    //     println!("3 + 4 = {}", result.value());
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    // file_path = ?
+    // 只有等全部解析加载完成我们才能得知要运行哪些文件哦
+    // if let Err(error) = runtime.block_on(run_js(file_path)) {
+    //     eprintln!("error: {error}");
     // }
 }
 
 pub fn shutdown() {
-    // unsafe { v8::V8::dispose() };
-    // v8::V8::dispose_platform();
+
 }
