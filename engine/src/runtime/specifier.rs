7c3bdb278d1c77716908d6d437154dd1de034915
laper32 <laper32@outlook.com>
2024-08-22 15:08:14 +0800

    wip

---
+++ engine/src/runtime/specifier.rs
@@ -0,0 +1,149 @@
+/// 这里应当分为如下几个部分：
+/// 1. Rift内部捆绑
+/// 2. 第一方插件
+/// 3. User目录的插件
+/// 4. 项目内部的插件
+/// 5. 项目内部作为Util的脚本
+use std::path::{Path, PathBuf};
+
+use relative_path::RelativePathBuf;
+
+/// An `import` specifier referring to a file within the current project.
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum ModuleLocalImportSpecifier {
+    Relative(String),
+    ProjectRoot(String),
+}
+
+impl std::fmt::Display for ModuleLocalImportSpecifier {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ModuleLocalImportSpecifier::Relative(path) => write!(f, "{path}"),
+            ModuleLocalImportSpecifier::ProjectRoot(path) => write!(f, "/{path}"),
+        }
+    }
+}
+
+impl std::str::FromStr for ModuleLocalImportSpecifier {
+    type Err = anyhow::Error;
+
+    fn from_str(specifier: &str) -> Result<Self, Self::Err> {
+        if specifier == "."
+            || specifier == ".."
+            || specifier.starts_with("./")
+            || specifier.starts_with("../")
+        {
+            return Ok(Self::Relative(specifier.to_string()));
+        } else if let Some(project_root_subpath) = specifier.strip_prefix('/') {
+            return Ok(Self::ProjectRoot(project_root_subpath.to_string()));
+        } else {
+            anyhow::bail!("Invalid module specifier: {}", specifier)
+        }
+    }
+}
+
+// =================================================================================
+
+/// A specifier from an `import` statement in a JavaScript module. Can
+/// be resolved to a module specifier using the `resolve` function.
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum ModuleImportSpecifier {
+    Local(ModuleLocalImportSpecifier),
+    External(String),
+}
+
+impl std::fmt::Display for ModuleImportSpecifier {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ModuleImportSpecifier::Local(specifier) => write!(f, "{}", specifier),
+            ModuleImportSpecifier::External(specifier) => write!(f, "{}", specifier),
+        }
+    }
+}
+
+// =============================================================================
+
+/// A specifier for a Rift module, either from the filesystem or
+/// from the internal runtime package. A module specifier can be converted
+/// from/to a URL.
+#[derive(
+    Debug, Clone, PartialEq, Eq, Hash, serde_with::DeserializeFromStr, serde_with::SerializeDisplay,
+)]
+pub enum ModuleSpecifier {
+    Runtime { subpath: RelativePathBuf },
+    File { path: PathBuf },
+}
+
+impl ModuleSpecifier {
+    pub fn from_path(path: &Path) -> Self {
+        Self::File {
+            path: path.to_owned(),
+        }
+    }
+}
+
+impl TryFrom<&'_ url::Url> for ModuleSpecifier {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &'_ url::Url) -> Result<Self, Self::Error> {
+        match value.scheme() {
+            "file" => {
+                let path = value
+                    .to_file_path()
+                    .map_err(|_| anyhow::anyhow!("Invalid file URL: {}", value))?;
+                Ok(Self::File { path })
+            }
+            "rift" => {
+                anyhow::ensure!(!value.has_host(), "invalid specifier: {}", value);
+                let subpath = RelativePathBuf::from(value.path().trim_start_matches('/'));
+                Ok(Self::Runtime { subpath })
+            }
+            _ => anyhow::bail!("Unsupported URL scheme: {}", value.scheme()),
+        }
+    }
+}
+
+impl TryFrom<url::Url> for ModuleSpecifier {
+    type Error = anyhow::Error;
+
+    fn try_from(value: url::Url) -> Result<Self, Self::Error> {
+        Self::try_from(&value)
+    }
+}
+
+impl From<&'_ ModuleSpecifier> for url::Url {
+    fn from(value: &'_ ModuleSpecifier) -> Self {
+        match value {
+            ModuleSpecifier::Runtime { subpath } => {
+                let mut url: url::Url = "rift:///".parse().expect("failed to build URL");
+                url.set_path(subpath.as_str());
+                url
+            }
+            ModuleSpecifier::File { path } => {
+                url::Url::from_file_path(path).expect("failed to build URL")
+            }
+        }
+    }
+}
+
+impl From<ModuleSpecifier> for url::Url {
+    fn from(value: ModuleSpecifier) -> Self {
+        Self::from(&value)
+    }
+}
+
+impl std::str::FromStr for ModuleSpecifier {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let url: url::Url = s.parse()?;
+        let specifier = url.try_into()?;
+        Ok(specifier)
+    }
+}
+
+impl std::fmt::Display for ModuleSpecifier {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", url::Url::from(self))
+    }
+}
--- sample/08_plugin/main.ts
+++ sample/08_plugin/main.ts
@@ -1,3 +1,16 @@
-async function main() {
+namespace rift {
+    export enum EventIdentity {
+        Load,
+        Unload,
+        AllLoad
+    } // enum 遇到自定义事件直接炸
 
+
+    export function RegisterEvent(event: EventIdentity, callback: Function) {
+        // rift.RegisterEvent(event, callback)
+    }
+}
+async function main() {
+    rift.RegisterEvent(rift.EventIdentity.Load, async () => {
+    })
 }
